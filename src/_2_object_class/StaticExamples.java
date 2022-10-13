d6d83a59f8b07f13dedccd6785d7f90c1cef5485
Aditya Sahu <sahuaditya083@gmail.com>
2022-10-13 10:02:51 +0530

    Object and class, static keyword

---
+++ src/_2_object_class/StaticExamples.java
@@ -0,0 +1,37 @@
+package _2_object_class;
+
+public class StaticExamples {
+    public static void main(String[] args) {
+        Student s = new Student(123, "Aditya Sahu");
+        s.printVal();
+
+        Counter c1 = new Counter();
+        Counter c2 = new Counter();
+        Counter c3 = new Counter();
+    }
+}
+
+
+class Student {
+    long id;
+    String name;
+    static String collegeName = "IIIT";
+
+    public Student(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    void printVal() {
+        System.out.println(collegeName + " " + name + " " + id);
+    }
+}
+
+class Counter {
+    static int count = 0;
+
+    public Counter() {
+        count++;
+        System.out.println(count);
+    }
+}
